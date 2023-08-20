3fc1bbd242ddb7143b26d9b85edcb88be44cca5a
minhthai9520 <digi.thainguyen00@gmail.com>
2023-08-20 23:15:00 +0700

    Minthai9520

---
+++ Projects_HomeWork/Homework/Homework/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Using System;*/
+namespace Homework_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime LocalTime = DateTime.Now;
+            Console.WriteLine("DateTime Now is: "+ LocalTime);
+
+        }
+    } 
+}
---
+++ Projects_HomeWork/Homework/Homework_02/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Homework_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float width;
+            float height;
+            float aren;
+            Console.WriteLine("Enter Width: ");
+            width = float.Parse(Console.ReadLine());
+            Console.WriteLine("Enter height: ");
+            height = float.Parse(Console.ReadLine());
+            aren = width * height;
+            Console.Write("Aren is: " + aren);
+
+        }
+    }
+}
