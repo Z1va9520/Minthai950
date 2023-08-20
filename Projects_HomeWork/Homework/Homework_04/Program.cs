48962b9b89090b40475cb97ebd946a4ada2be78c
minhthai9520 <digi.thainguyen00@gmail.com>
2023-08-20 23:22:30 +0700

    Minthai950

--- Projects_HomeWork/Homework/Homework_04/Program.cs
+++ Projects_HomeWork/Homework/Homework_04/Program.cs
@@ -1,1 +1,16 @@
-﻿namespace Homework_04{    class Program    {        static void Main(string[] agrs)        {            float usa;            float usatovn;            float vn = 23000f;            Console.WriteLine("Enter your money:");            usa = float.Parse(Console.ReadLine());            usatovn = usa * vn;            Console.WriteLine("Vn to UsA: ");        }           }}
+﻿namespace Homework_04
+{
+    class Program
+    {
+        static void Main(string[] agrs)
+        {
+            float usa;
+            float usatovn;
+            float vn = 23000f;
+            Console.WriteLine("Enter your money:");
+            usa = float.Parse(Console.ReadLine());
+            usatovn = usa * vn;
+            Console.WriteLine("Vn to UsA: ");
+        }       
+    }
+}
