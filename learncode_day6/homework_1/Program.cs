eabee2e768d611f9464d3c61475e0b89edcf8c9e
minhthai9520 <digi.thainguyen00@gmail.com>
2023-09-05 21:02:17 +0700

    learncode_day6

---
+++ learncode_day6/homework_1/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace learncode_day6
+{
+    class Program
+    {
+         public static int sum(int i1, int i2) {
+            int result = 0;
+            for (int i = i1; i <= i2; i++)
+               result += i;
+            return result;
+         }
+
+         public static void Main(String[] args) {
+            Console.WriteLine("Sum from 1 to 10 is " + sum(1, 10));
+            Console.WriteLine("Sum from 20 to 37 is " + sum(20, 37));
+            Console.WriteLine("Sum from 35 to 49 is " + sum(35, 49));
+         }
+    }
+}
---
+++ learncode_day6/homework_2/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace learncode_day6
+{
+    class Program
+    {
+        public static void Main(string[] agrs)
+        {
+            int j = 7;
+            int i = 3;
+            int k = Max(j, i);
+            Console.WriteLine($"the maxium {j} and {i} is {k}");
+        }
+
+        public static int Max(int i1, int i2)
+        {
+            int resule;
+            if (i1 > i2)
+            {
+                resule = i1;
+            }
+            else if (i1 < i2)
+            {
+                resule = i2;
+            }
+            else
+            {
+                resule = i1;
+            }
+
+            return resule;
+        }
+
+    }
+}
